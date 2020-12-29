1b5fd5cdc7f69adc78678899affa76b34cb139b3
jotkafomat <k-j@xl.wp.pl>
2020-12-29 20:37:21 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Krzysztof Jankowski on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
