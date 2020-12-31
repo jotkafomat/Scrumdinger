6166246bf34f4104d7585432b3015af1fe82064b
jotkafomat <k-j@xl.wp.pl>
2020-12-31 11:35:30 +0000

    add navigation to list

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -12,16 +12,25 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.color)
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
             }
         }
+        .navigationBarTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+            Image(systemName: "plus")
+        }))
+        
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.data)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
     }
 }
